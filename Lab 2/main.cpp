ca25c4d024bf6ee063702427ee257fc0677fb705
Hazzan <85147922+Igho-S@users.noreply.github.com>
2023-12-23 14:51:46 +0700

    1

--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
 #include <csignal>
+#include <sys/select.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <algorithm>
-#include <thread>
-#include <mutex>
+
+
+//Хаззан Омотола 932024
+
+#define PORT 25252
 
 std::mutex signalMutex;
 volatile sig_atomic_t wasSigHup = 0;
@@ -15,17 +21,29 @@ void sigHupHandler(int signal) {
     wasSigHup = 1;
 }
 
+void handleClient(int clientSocket) {
+    char buffer[1024];
+    int bytesRead;
+
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        // To process and handle the received data
+        send(clientSocket, buffer, bytesRead, 0);
+    }
+
+    close(clientSocket);
+}
+
 int main() {
-    int superSocket, newSocket;
+    int serverSocket, newSocket;
     std::vector<int> clientSockets;
     struct sockaddr_in serverAddress;
     socklen_t clientAddressSize = sizeof(struct sockaddr_in);
-    std::vector<std::thread> clientThreads;
+    fd_set fds; // declares an fd_set structure that will be used with pselect to monitor file descriptors.
     struct sigaction sa;
 
     // To create a socket for listening
-    superSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (superSocket == -1) {
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
         std::cerr << "Failed to create socket" << std::endl;
         return 1;
     }
@@ -33,21 +51,22 @@ int main() {
     // Here to bind the socket to the specified port
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(25252);
-    if (bind(superSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+    serverAddress.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         std::cerr << "Failed to bind to port" << std::endl;
-        close(superSocket);
+        close(serverSocket);
         return 1;
     }
 
     // To listen for incoming connections
-    if (listen(superSocket, 5) == -1) {
+    if (listen(serverSocket, 5) == -1) {
         std::cerr << "Failed to listen for connections" << std::endl;
-        close(superSocket);
+        close(serverSocket);
         return 1;
     }
 
-    std::cout << "Server listening on port 25252" << std::endl;
+    std::cout << "Server listening on port " << PORT << std::endl;
 
     // Here, to register signal handler for SIGHUP
     // Sets the handler to sigHupHandler, and ensures that system calls are restarted if interrupted by this signal.
@@ -56,59 +75,53 @@ int main() {
     sa.sa_flags |= SA_RESTART;
     sigaction(SIGHUP, &sa, nullptr);
 
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+
     while (true) {
-        fd_set fds;
         FD_ZERO(&fds);
-        FD_SET(superSocket, &fds); // clears the file descriptor set fds and adds the superSocket to it.
+        FD_SET(serverSocket, &fds);
 
-        int maxFd = superSocket; // Update maxFd based on established connections
+        int maxFd = serverSocket;
 
-        {
-            std::lock_guard<std::mutex> lock(signalMutex);
-            if (wasSigHup) {
-                std::cout << "SIGHUP signal was processed" << std::endl;
-                wasSigHup = 0;
+        int result = pselect(maxFd + 1, &fds, nullptr, nullptr, nullptr, &origMask);
+
+        if (result == -1) {
+            if (errno == EINTR) {
+                std::cout << "Received SIGHUP signal" << std::endl;
+                {
+                    std::lock_guard<std::mutex> lock(signalMutex);
+                    wasSigHup = 0;
+                }
+            } else {
+                std::cerr << "pselect error" << std::endl;
             }
         }
 
-        if (select(maxFd + 1, &fds, nullptr, nullptr, nullptr) == -1) {
-            std::cerr << "select error" << std::endl;
-            continue;
+        if (wasSigHup) {
+            std::cout << "SIGHUP signal was processed" << std::endl;
         }
 
-        if (FD_ISSET(superSocket, &fds)) {
-            newSocket = accept(superSocket, nullptr, nullptr);
+        if (FD_ISSET(serverSocket, &fds)) {
+            newSocket = accept(serverSocket, nullptr, nullptr);
+
+            if (newSocket == -1) {
+                std::cerr << "Failed to accept connection" << std::endl;
+                continue;
+            }
 
             // Here, to add the new client socket to the vector
             clientSockets.push_back(newSocket);
 
             std::cout << "New client connected." << std::endl;
 
-            // Create a new thread for each client
-            clientThreads.emplace_back([newSocket, &clientSockets]() {
-                char buffer[1024];
-                int bytesRead;
-                while ((bytesRead = recv(newSocket, buffer, sizeof(buffer), 0)) > 0) {
-                    // Process and handle the received data
-                    send(newSocket, buffer, bytesRead, 0);
-                }
-
-                // To delete the disconnected client socket from the vector
-                clientSockets.erase(std::remove(clientSockets.begin(), clientSockets.end(), newSocket), clientSockets.end());
-
-                std::cout << "Client disconnected." << std::endl;
-
-                close(newSocket); // Close the client socket
-            });
+            // Here, to handle the new client in a separate thread
+            std::thread(handleClient, newSocket).detach();
         }
-
-        // Cleanup finished threads
-        clientThreads.erase(std::remove_if(clientThreads.begin(), clientThreads.end(),
-                                           [](const std::thread& t) { return !t.joinable(); }),
-                            clientThreads.end());
     }
 
-    close(superSocket);
+    close(serverSocket);
 
     return 0;
 }
